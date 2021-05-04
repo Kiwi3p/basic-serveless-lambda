8015be9a2ae558eab4846169f3c885e7c5b3030d
John Transue <johnktransue@gmail.com>
2021-05-04 15:17:33 -0400

    trying this

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports.generateRandomNumber = async (event) => {
+module.exports.generateRandomNumber = (event) => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("the random generated integer is:", randomNumber);
   return randomNumber;
