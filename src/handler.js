ef4a7fb7f8bc975010d4d2d0be328577f8ec0693
John Transue <johnktransue@gmail.com>
2021-05-04 15:36:26 -0400

    fixed src

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@
 
 module.exports.generateRandomNumber = async (event) => {
   const randomNumber = parseInt(Math.random() * 100);
-  console.log("test";)
+  console.log("test");
   console.log("the random generated integer is:", randomNumber);
   return randomNumber;
 };
