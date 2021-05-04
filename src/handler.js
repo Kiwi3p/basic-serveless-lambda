5416919dc1e9220dbabddd1298b47333369e1355
John Transue <johnktransue@gmail.com>
2021-05-04 15:30:45 -0400

    new console log

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,8 @@
 "use strict";
 
-module.exports.generateRandomNumber = (event) => {
+module.exports.generateRandomNumber = async (event) => {
   const randomNumber = parseInt(Math.random() * 100);
+  console.log("test";)
   console.log("the random generated integer is:", randomNumber);
   return randomNumber;
 };
